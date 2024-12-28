a440bc0310468ce320152ef27dc7b7365935ebe9
bhushanmandava <162395532+bhushanmandava@users.noreply.github.com>
2024-12-28 12:06:52 -0500

    updated path for main.tsx

--- src/main.js
+++ src/main.js
@@ -3,5 +3,4 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import './main.css';
-
 createRoot(document.getElementById("root")).render(_jsx(StrictMode, { children: _jsx(App, {}) }));
