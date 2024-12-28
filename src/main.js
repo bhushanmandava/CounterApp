c85e5f8c9df9d021ac347e9766b22916ac485de8
bhushanmandava <162395532+bhushanmandava@users.noreply.github.com>
2024-12-28 11:40:23 -0500

    chagnge path for main.cdss

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { jsx as _jsx } from "react/jsx-runtime";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import 'CounterApp/main.css';
+import './main.css';
+
 createRoot(document.getElementById("root")).render(_jsx(StrictMode, { children: _jsx(App, {}) }));
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import '/Users/bhushanchowdary/Documents/GitHub/CounterApp/main.css';
+import 'CounterApp/main.css';
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
