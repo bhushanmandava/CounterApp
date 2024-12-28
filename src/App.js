17e32c745feca5bb54bcba4f1c1dccd0581f8231
bhushanmandava <162395532+bhushanmandava@users.noreply.github.com>
2024-12-28 13:00:34 -0500

    hargav

--- src/App.js
+++ src/App.js
@@ -4,5 +4,5 @@ export default function App() {
     const [count, setCount] = useState(0);
     const inc = () => setCount(count => count + 1);
     const dec = () => setCount(count => count - 1);
-    return (_jsxs(_Fragment, { children: [_jsxs("div", { children: [_jsx("button", { onClick: dec, children: "-" }), _jsx("div", { children: count }), _jsx("button", { onClick: inc, children: "+" })] }), _jsx("div", { children: "Hello from bhushan" })] }));
+    return (_jsxs(_Fragment, { children: [_jsxs("div", { children: [_jsx("button", { onClick: dec, children: "-" }), _jsx("div", { children: count }), _jsx("button", { onClick: inc, children: "+" })] }), _jsx("div", { children: "Hello from hargv" })] }));
 }
