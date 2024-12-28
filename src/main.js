d5bf95629cc46b86fea57ea4143dc429b500c8f2
bhushanmandava <162395532+bhushanmandava@users.noreply.github.com>
2024-12-28 11:27:51 -0500

    Counter app

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import { jsx as _jsx } from "react/jsx-runtime";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import '/Users/bhushanchowdary/Documents/GitHub/CounterApp/main.css';
+import 'CounterApp/main.css';
 createRoot(document.getElementById("root")).render(_jsx(StrictMode, { children: _jsx(App, {}) }));
